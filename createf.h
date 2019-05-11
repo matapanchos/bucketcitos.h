5ca23662c3ffeda15b485c4345351ea1ee9baca1
Joe-08 <joe_andre0710@hotmail.com>
2019-05-11 03:19:30 -0500

    Te llena una linea de txt por runeada

--- createf.h
+++ createf.h
@@ -6,17 +6,35 @@
 
 using namespace std;
 
-template <typename T>
-double timer(T (*funcion)(T), T arg) {
-    auto start = chrono::high_resolution_clock::now();
-    T f = funcion(arg);
-    auto end = chrono::high_resolution_clock::now();
-
-    chrono::duration<double> runtime = end - start;
-    return runtime.count();
+template <typename M>
+double prom_array(vector<M> arr) {
+    double promedio = 0;
+    for (int i = 0; i < arr.size(); i++) {
+        promedio += arr[i];
+    }
+    promedio /= arr.size();
+    return promedio;
 }
 
-void createfile(string filename, vector<double> time, int n) {
+template <typename T>
+double timer(T (*funcion)(T), T arg, int num_tests) {
+    vector<double> promarr;
+    int i = 0;
+    chrono::time_point<chrono::high_resolution_clock> start, end;
+    while (i < num_tests) {
+        start = chrono::high_resolution_clock::now();
+        T f = funcion(arg);
+        end = chrono::high_resolution_clock::now();
+
+        chrono::duration<double> runtime = end - start;
+        promarr.push_back(runtime.count());
+        i++;
+    }
+
+    return prom_array(promarr);
+}
+
+void createfile(string filename, vector<double> time, int arg) {
     fstream file;
     file.open(filename, ios::app);
 
@@ -24,15 +42,13 @@ void createfile(string filename, vector<double> time, int n) {
         cout << "Error in creating file" << endl;
         return;
     }
-    
     for (int i = 0; i < time.size(); i++) {
         if (i == 0)
-            file << n << " ";
+            file << arg << " ";
         if (i != time.size() - 1)
             file << fixed << setprecision(10) << time[i] << " ";
         else
             file << fixed << setprecision(10) << time[i] << endl;
-
     }
     file.close();
 }
