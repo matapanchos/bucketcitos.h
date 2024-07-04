6c230244146cd583c7037575a9c221551656bbc7
matapanchos <cares.guero@gmail.com>
2024-07-04 00:53:35 -0500

    Add bucketcitos logos

---
+++ bucketcitos_logos/affect_color.py
@@ -0,0 +1,17 @@
+c1_1 = -0.2
+c1_2 = -17.6
+c1_3 = -23.9
+
+c2_1 = -0.2
+c2_2 = -29.1
+c2_3 = -9.4
+
+while (True):
+	base_h = float(input())
+	base_s = float(input())
+	base_v = float(input())
+
+	print(base_h+c2_1)
+	print(base_s+c2_2)
+	print(base_v+c2_3)
+	print("NEW ENTRY")
